a236181c8ebefea70216fd6bfe4b7e19647d798c
Avia-Cheriker <avia.cheriker@gmail.com>
2017-03-14 11:36:15 +0200

    chnages

---
+++ my-app/src/app/nav-menu/nav-menu.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component(
+{
+  selector: 'app-nav-menu',
+  templateUrl: './nav-menu.component.html',
+  styleUrls: ['./nav-menu.component.css']
+})
+export class NavMenuComponent implements OnInit 
+{
+  buttons = [
+              { id: "home" , txt: "Home" },
+              { id: "page" , txt: "Page" },
+              { id: "about" , txt: "About" }, 
+              { id: "contactUs" , txt: "Contact Us" }
+             ]
+             
+  constructor() { }
+
+  ngOnInit() 
+  {
+  }
+
+}
